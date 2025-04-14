6bc836cda1a6eab6be58edde467128c5fb8da632
Bogdan Kurylo <lo067803@outlook.com>
2025-04-14 14:19:24 +0300

    login page, register page

--- mvc_project/Controllers/HomeController.cs
+++ mvc_project/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace mvc_project.Controllers
             return View();
         }
 
+        public IActionResult Info()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ mvc_project/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_project.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Console.WriteLine("Products");
+
+            return View();
+        }
+    }
+}
