c3a03a0ebff38094c2335dda3907505f442d3381
Bogdan Kurylo <lo067803@outlook.com>
2025-04-23 01:18:40 +0300

    реалізація CRUD для категорій

--- mvc_project/Controllers/CategoryController.cs
+++ mvc_project/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using mvc_project.Data;
 using mvc_project.Models;
 
@@ -24,12 +25,44 @@ namespace mvc_project.Controllers
             return View();
         }
 
+        public IActionResult Edit(string id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+
+        public IActionResult Delete(string id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            return View(category);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category model)
         {
             model.Id = Guid.NewGuid().ToString();
             _context.Categories.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category model)
+        {
+            _context.Categories.Update(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category model)
+        {
+            _context.Categories.Remove(model);
             _context.SaveChanges();
 
             return RedirectToAction("Index");
